f16462b84b007ba5231f2d6b7b779fe1a695229d
Pelle Braendgaard <pelle@stakeventures.com>
2015-10-31 21:09:12 -0600

    Update project info

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject bux "0.3.0"
   :description "Simple money handling library"
+  :url "https://github.com/pelle/bux"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.145" :scope "provided"]]
 
